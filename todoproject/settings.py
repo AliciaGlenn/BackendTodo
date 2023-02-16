f9dc56673884d971764c8772c5db477ea1892862
AliciaGlenn <Alicia@AliciaGlenn.com>
2023-02-15 19:21:52 -0500

    cors middleware

--- todoproject/settings.py
+++ todoproject/settings.py
@@ -58,6 +58,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
      'django.middleware.clickjacking.XFrameOptionsMiddleware',
+     'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'todoproject.urls'
